7f2b796b772a678fcaa9cf1735d1d7c7bbe579a4
Joseph Higgins <joehiggins07@gmail.com>
2017-09-15 16:05:03 -0700

    tabs to spaces in application.py

--- application.py
+++ application.py
@@ -8,6 +8,6 @@ Created on Tue Aug 15 15:59:15 2017
 #!flask/bin/python
 from application import application
 if __name__ == '__main__':
-	application.debug = True
-	port = int(os.environ.get("PORT", 5000))
+    application.debug = True
+    port = int(os.environ.get("PORT", 5000))
     application.run(host='0.0.0.0', port=port)
